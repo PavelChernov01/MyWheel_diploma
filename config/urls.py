06f1a6cc51d2122111aa856fa8adb20f32abbde1
Pavel <pv_chernov@mail.ru>
2026-07-19 14:35:12 +0300

    Stage 8 completed: Password_reset

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from cars.views import car_list, car_detail, car_create
 from accounts.views import login_view, logout_view, register_view, profile_view
@@ -31,6 +32,30 @@ urlpatterns = [
     path('favorites/', favorites_list, name='favorites_list'),
     path('favorites/add/<int:pk>/', favorites_add, name='favorites_add'),
     path('favorites/remove/<int:pk>/', favorites_remove, name='favorites_remove'),
+
+    # Восстановление пароля
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='accounts/password_reset.html',
+             email_template_name='accounts/password_reset_email.html',
+             subject_template_name='accounts/password_reset_subject.txt'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
